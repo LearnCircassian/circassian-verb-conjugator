cc678ac5be94b1633ba55a6a121844f09f07e7fe
Nart <113383731+nartna123@users.noreply.github.com>
2024-08-03 00:02:24 +0300

    .

--- index.ts
+++ index.ts
@@ -1,1 +1,76 @@
-console.log("Hello via Bun!");
+
+enum Tense {
+    Past,
+    Present,
+    Future,
+}
+
+enum VerbType {
+    MonovalentIntransitive,
+    Transitive,
+}
+
+enum Person {
+    Singular1stPerson,
+    Singular2stPerson,
+    Singular3rdPerson,
+    Plural1stPerson,
+    Plural2stPerson,
+    Plural3rdPerson,
+    None,
+}
+
+type Verb = {
+    root: string;
+    verbType: VerbType;
+    absolutiveMarker: Person;
+    ergativeMarker?: Person;
+    obliqueMarker?: Person;
+    tense?: Tense;
+    negative?: boolean;
+}
+
+class VerbObject {
+    private readonly verbConfigs: Verb;
+
+    public constructor(verb: Verb) {
+        this.verbConfigs = {
+            ergativeMarker: Person.None,
+            obliqueMarker: Person.None,
+            negative: false,
+            ...verb,
+        }
+    }
+
+    public getVerb(): Verb {
+        return this.verbConfigs;
+    }
+}
+
+
+
+function generateVerbTable(infinitiveForm: string, verbType: VerbType): Verb[] {
+    const list = [] as Verb[];
+    const root = infinitiveForm.slice(0, -1); // remove last letter -н
+
+    switch (verbType) {
+        case VerbType.MonovalentIntransitive:
+            const res = getMonovalentIntransitiveVerbConjucation(root);
+            break;
+    }
+
+    return list;
+}
+
+function getMonovalentIntransitiveVerbConjucation(root: string): Verb[] {
+    const list = [] as Verb[];
+    // сэплъэ
+    let v = new VerbObject({
+        root:`сэ${root}`,
+        verbType: VerbType.MonovalentIntransitive,
+        absolutiveMarker: Person.Singular1stPerson,
+    });
+    list.push(v.getVerb());
+
+    return list;
+}
